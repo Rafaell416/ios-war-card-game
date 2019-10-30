b3ac3a00c837ce2327dc5033e408c12e1bcc16ce
Rafael Villarreal <rvillarreal416@gmail.com>
2019-10-30 13:21:56 -0500

    initial assets

---
+++ WarCardGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by Rafael Villarreal on 10/30/19.
+//  Copyright © 2019 Rafael Villarreal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
